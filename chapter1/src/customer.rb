da902246fa1ebbef90780f445a737559ee716479
Jais Cheema <jaischeema@gmail.com>
2017-07-11 17:35:34 +1000

    Replace Temp with Query

--- chapter1/src/customer.rb
+++ chapter1/src/customer.rb
@@ -16,15 +16,13 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{name}\n"
     @rentals.each do |rental|
-      this_amount = rental.charge
-
       frequent_renter_points += 1
       if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
         frequent_renter_points += 1
       end
 
-      result += "\t" + rental.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
+      result += "\t" + rental.movie.title + "\t" + rental.charge.to_s + "\n"
+      total_amount += rental.charge
     end
 
     result += "Amount owed is #{total_amount}\n"
